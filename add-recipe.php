b505924adae3586ed8e5d9277df6aee25e05f0ac
Hien Nguyen <hientrungnguyen@gmail.com>
2017-12-06 00:23:23 -0600

    experimenting queries to get id of chosen inputs

--- add-recipe.php
+++ add-recipe.php
@@ -1,4 +1,37 @@
 <?php
+ini_set('disply_errors', 'On'); error_reporting(E_ALL | E_STRICT);
+include 'functions/functions.php';
+
+if ($_POST) {
+    echo 'Post is set </br>';
+
+// Set variables to input data
+$soapname = $_POST['soapname'];
+$ingredient = $_POST['ingredient'];
+$amount = $_POST['amount'];
+$unit = $_POST['unit'];
+echo $soapname . '</br>';
+echo $ingredient . '</br>';
+echo $amount . '</br>';
+echo $unit . '</br>';
+
+$qry = "SELECT id as 'ingredID'
+        FROM ingredients
+        WHERE name = '$ingredient'";
+
+$return = mysql($qry);
+
+while ($row = mysqli_fetch_assoc($return)) {
+    $ingredarr[] = array (
+        'ingred_id' => $row['ingredID']
+    );
+}
+
+// print_r($ingredarr);
+foreach ($ingredarr as $x) {
+    echo $x['ingred_id']; 
+    }
+}
 ?>
 
 
@@ -9,7 +42,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Add New Recipe</title>
-    <link rel="stylesheet" href="styles/addrecipe-style.css">
+    <!-- <link rel="stylesheet" href="styles/addrecipe-style.css"> -->
 </head>
 <body>
    <div class="container">
@@ -21,25 +54,28 @@
                <label for="name">
                   <span>Soap Name: </span>
                </label>
-               <input type="text" id="name" name="username">
+               <input type="text" id="soapinput" name="soapname">
             </p>
             <p>
-               <label for="mail">
+               <label for="ingredient">
                   <span>Ingredient: </span>
                </label>
-               <input type="email" id="mail" name="usermail">
+               <input type="text" id="ingredientinput" name="ingredient">
             </p>
             <p>
-               <label for="pwd">
+               <label for="amount">
                   <span>Amount: </span>
                </label>
-               <input type="password" id="pwd" name="password">
+               <input type="number" id="amountinput" name="amount">
             </p>
             <p>
                <label for="unit">
                   <span>Unit: </span>
                </label>
-               <input type="password" id="unit" name="password">
+               <input type="text" id="unitinput" name="unit">
+            </p>
+            <p>
+               <button type="submit">Submit</button>
             </p>
          </section> 
       </form>
